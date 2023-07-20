41c7b8562af0941dabdcd9db92bd657704f2bd5c
Vitor Paiola <vitinho.souza1234@gmail.com>
2023-07-20 16:05:28 -0300

    First concepts of routes with react

---
+++ src/pages/Home/index.js
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom"
+
+function Home() {
+    return (
+        <div>
+            <h1>Bem-vindo a página HOME</h1>
+            <span>Sujeito Programador</span> <br /> <br />
+
+            <Link to="/sobre">Sobre</Link> <br />
+            <Link to="/contato">Contato</Link> <br />
+
+            <hr />
+
+            <Link to="/produto/123">Acessar produto 123</Link>
+
+        </div>
+    );
+}
+
+export default Home;
---
+++ src/pages/Produto/index.js
@@ -0,0 +1,17 @@
+import { useParams } from "react-router-dom"
+ 
+function Produto() {
+
+    const { id } = useParams()
+
+    return (
+        <div>
+            <h2>Página detalhe do produto</h2>
+            <span>
+                Meu produto é { id }
+            </span>
+        </div>
+    )
+}
+
+export default Produto
